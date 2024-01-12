8ceca947b039cd404174037acd917d9eda61ac3b
unknown <Lavanya243@outlook.com>
2024-01-11 21:45:37 -0300

    updated action

--- Components/Product.js
+++ Components/Product.js
@@ -1,8 +1,16 @@
 import {View, Text, Image, Button} from 'react-native';
 import React from 'react';
+import {useDispatch} from 'react-redux';
+import {addToCart} from './redux/Action';
 
 export default function Product(props) {
   const item = props.item;
+  const dispatch = useDispatch();
+
+  const handleAddToCart = () => {
+    console.warn('called', item);
+    dispatch(addToCart(item));
+  };
   return (
     <View
       style={{
@@ -14,8 +22,11 @@ export default function Product(props) {
       <Text style={{fontSize: 24, margin: 4}}>{item.name}</Text>
       <Text style={{fontSize: 24, margin: 4}}>{item.price}</Text>
       <Text style={{fontSize: 24, margin: 4}}>{item.color}</Text>
-      <Image style={{width: 200, height: 200}} source={{uri: item.image}} />
-      <Button title="Add to cart" />
+      <Image
+        style={{width: 200, height: 200, marginBottom: 10}}
+        source={{uri: item.image}}
+      />
+      <Button title="Add to cart" onPress={() => handleAddToCart(item)} />
     </View>
   );
 }
